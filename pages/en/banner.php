d1fe8526aab6ce0f6e087d165ef99bfd66cae510
ajlich <ajla.zulovic55@gmail.com>
2023-08-06 21:25:41 +0200

    bannerPicture

--- pages/en/banner.php
+++ pages/en/banner.php
@@ -12,7 +12,7 @@
         <div class="container">
         <button id="close">&times;</button>
         <a href="https://www.samsung.com/us/about-us/brand-identity/logo/" class="popup-link" id="popupLink" target="_blank"> 
-            <img src="slike/samsungLogo.avif" alt="Reklamni banner" class="popup-img">
+            <img src="slike/slika.png" alt="Reklamni banner" class="popup-img">
         </a>
         </div>
     </div>
